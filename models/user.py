9253433bac705fe4fc7aea16ffcd795554512f2d
Luis Sanjuan <luiscode92@gmail.com>
2020-08-12 17:43:20 -0500

    pep8 fixed

--- models/user.py
+++ models/user.py
@@ -14,4 +14,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship('Place', backref='user',
                           cascade='all, delete-orphan')
-    
