eb955f35c537d6e33957aaf5fa7a509421f73bf1
Luis Sanjuan <luiscode92@gmail.com>
2020-08-12 17:25:44 -0500

    add city and place

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -18,3 +19,9 @@ class City(BaseModel, Base):
         ForeignKey('states.id'),
         nullable=False
     )
+
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
