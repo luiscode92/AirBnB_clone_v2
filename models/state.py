2ca2d4b499e86065d7995d6d82fbc2d9aeb11c48
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-13 13:29:53 -0500

    modified files

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -15,16 +16,17 @@ class State(BaseModel, Base):
         nullable=False
     )
 
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """ cities getter """
+            from models import storage
 
-    @property
-    def cities(self):
-        """ cities getter """
-        from models import storage
-
-        all_cts = storage.all(City)
-        sts_cts = []
-        for city in all_cts.values():
-            if self.id == city.state_id:
-                sts_cts.append(city)
-        return sts_cts
+            all_cts = storage.all(City)
+            sts_cts = []
+            for city in all_cts.values():
+                if self.id == city.state_id:
+                    sts_cts.append(city)
+            return sts_cts
