092850ad7ad9fe7abfd33b2d6ce1c65f9831af9c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-17 21:54:46 -0500

    distributes an archive to your web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,28 +5,57 @@ from fabric.api import run, env, put
 from os.path import exists
 import re
 
-env.hosts = ['34.73.198.19', '54.90.192.199']
+env.hosts = ['54.90.192.199', '34.73.198.19']
 
 
 def do_deploy(archive_path):
     """ Deploys archive """
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
 
     arch_name = re.split('[/.]', archive_path)[1]
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}'.format(arch_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-            arch_name, arch_name))
-        run('rm /tmp/{}.tgz'.format(arch_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(arch_name, arch_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(arch_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(arch_name))
+    direc = '/data/web_static/releases'
 
-        return True
-    except Exception:
-        return False
+    stat = True
+
+    upload = put(archive_path, '/tmp/{}.tgz'.format(arch_name))
+    if upload.failed:
+        stat = False
+
+    di = run(
+        'sudo mkdir -p {}/{}/'.format(direc, arch_name))
+    if di.failed:
+        stat = False
+
+    unp = run(
+        'sudo tar -xzf /tmp/{}.tgz -C {}/{}/'
+        .format(arch_name, direc, arch_name))
+    if unp.failed:
+        stat = False
+
+    rem = run('sudo rm /tmp/{}.tgz'.format(arch_name))
+    if rem.failed:
+        stat = False
+
+    mov = run(
+        'sudo mv {}/{}/web_static/* {}/{}/'
+        .format(direc, arch_name, direc, arch_name))
+    if mov.failed:
+        stat = False
+
+    ren = run(
+        'sudo rm -rf {}/{}/web_static'.format(direc, arch_name))
+    if ren.failed:
+        stat = False
+
+    rep = run('sudo rm -rf /data/web_static/current')
+    if rep.failed:
+        stat = False
+
+    lin = run(
+        'sudo ln -s {}/{}/ /data/web_static/current'
+        .format(direc, arch_name))
+    if lin.failed:
+        stat = False
+
+    return stat
