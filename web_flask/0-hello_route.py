5569bc8e6ad326887864b2ec257baf689a290916
Luis Sanjuan <luiscode92@gmail.com>
2020-09-02 14:14:06 -0500

    fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,8 @@
 """
 Start a Flask web application
 """
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,5 +13,7 @@ def hello_hbnb():
     """ Print a message """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
