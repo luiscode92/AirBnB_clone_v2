195a357bc1827db524f3951f372b8cb9926de07e
Luis Sanjuan <luiscode92@gmail.com>
2020-09-03 15:32:37 -0500

    [added] states list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#/usr/bin/python3
+"""
+Start a Flask web appication
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__, template_folder='tem plates')
+
+
+@app.route('/state_list', strict_slashes=False)
+def states_list():
+    """Display HTML page with list of states"""
+    return render_template('7-states_list.html', 
+                            states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def remove_session(response_or_exc):
+    """Remove the curent SQLALchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
