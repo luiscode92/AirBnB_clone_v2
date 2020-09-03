16ff14555ed53d7c5a9c19e90861b3058fd41615
Luis Sanjuan <luiscode92@gmail.com>
2020-09-02 23:03:58 -0500

    added 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,7 +3,7 @@
 Start a Flask web application
 """
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/', strict_slashes=False)
